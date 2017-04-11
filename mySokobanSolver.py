6cfe0071bcfe146d399b4b64e4cbc63640bdd996
Matthew Miller <mnmiller1@me.com>
2017-04-11 10:31:23 +1000

    Conform to PEP8

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -1,20 +1,16 @@
-
-'''
-
+"""
 The partially defined functions and classes of this module 
 will be called by a marker script. 
 
-You should complete the functions and classes according to their specified interfaces.
- 
-
-'''
+You should complete the functions and classes according to their specified 
+interfaces.
+"""
 from math import sqrt
 
 import search
 from search import astar_graph_search as astar_graph
 
 import sokoban
-
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -27,6 +23,7 @@ def my_team():
     return [(8884731, 'Astrid', 'Jonelynas'),
             (8847436, 'Lindsay', 'Watt'),
             (9342401, 'Madeline', 'Miller')]
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -53,19 +50,20 @@ def taboo_cells(warehouse):
     ##         "INSERT YOUR CODE HERE"    
     raise NotImplementedError()
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 class SokobanPuzzle(search.Problem):
-    '''
+    """
     Class to represent a Sokoban puzzle.
     Your implementation should be compatible with the
     search functions of the provided module 'search.py'.
     
-    	Use the sliding puzzle and the pancake puzzle for inspiration!
-    
-    '''
-    ##         "INSERT YOUR CODE HERE"
+    Use the sliding puzzle and the pancake puzzle for inspiration!
+    """
+
+    # "INSERT YOUR CODE HERE"
 
     def __init__(self, warehouse):
         raise NotImplementedError()
@@ -78,12 +76,14 @@ class SokobanPuzzle(search.Problem):
         """
         raise NotImplementedError
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
 def check_action_seq(warehouse, action_seq):
-    '''
+    """
     
-    Determine if the sequence of actions listed in 'action_seq' is legal or not.
+    Determine if the sequence of actions listed in 'action_seq' is legal or not
     
     Important notes:
       - a legal sequence of actions does not necessarily solve the puzzle.
@@ -102,9 +102,9 @@ def check_action_seq(warehouse, action_seq):
                A string representing the state of the puzzle after applying
                the sequence of actions.  This must be the same string as the
                string returned by the method  Warehouse.__str__()
-    '''
+    """
 
-    ##         "INSERT YOUR CODE HERE"
+    # "INSERT YOUR CODE HERE"
 
     raise NotImplementedError()
 
@@ -112,7 +112,7 @@ def check_action_seq(warehouse, action_seq):
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def solve_sokoban_elem(warehouse):
-    '''    
+    """    
     This function should solve using elementary actions 
     the puzzle defined in a file.
     
@@ -121,20 +121,22 @@ def solve_sokoban_elem(warehouse):
     @return
         A list of strings.
         If puzzle cannot be solved return ['Impossible']
-        If a solution was found, return a list of elementary actions that solves
-            the given puzzle coded with 'Left', 'Right', 'Up', 'Down'
+        If a solution was found, return a list of elementary actions that 
+        solves the given puzzle coded with 'Left', 'Right', 'Up', 'Down'
             For example, ['Left', 'Down', Down','Right', 'Up', 'Down']
             If the puzzle is already in a goal state, simply return []
-    '''
+    """
 
-    ##         "INSERT YOUR CODE HERE"
+    # "INSERT YOUR CODE HERE"
 
     raise NotImplementedError()
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 offset_states = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 
 def can_go_there(warehouse, dst):
     """    
@@ -142,6 +144,7 @@ def can_go_there(warehouse, dst):
     without pushing any box.
     
     @param warehouse: a valid Warehouse object
+    @param dst: The destination tuple in (row,col)
 
     @return
       True if the worker can walk to cell dst=(row,col) without pushing any box
@@ -177,18 +180,20 @@ def can_go_there(warehouse, dst):
 
     return node is not None
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
 def solve_sokoban_macro(warehouse):
-    '''    
+    """    
     Solve using macro actions the puzzle defined in the warehouse passed as
     a parameter. A sequence of macro actions should be 
     represented by a list M of the form
             [ ((r1,c1), a1), ((r2,c2), a2), ..., ((rn,cn), an) ]
     For example M = [ ((3,4),'Left') , ((5,2),'Up'), ((12,4),'Down') ] 
-    means that the worker first goes the box at row 3 and column 4 and pushes it left,
-    then goes the box at row 5 and column 2 and pushes it up, and finally
-    goes the box at row 12 and column 4 and pushes it down.
+    means that the worker first goes the box at row 3 and column 4 and pushes
+    it left, then goes the box at row 5 and column 2 and pushes it up, and 
+    finally goes the box at row 12 and column 4 and pushes it down.
     
     @param warehouse: a valid Warehouse object
 
@@ -196,11 +201,10 @@ def solve_sokoban_macro(warehouse):
         If puzzle cannot be solved return ['Impossible']
         Otherwise return M a sequence of macro actions that solves the puzzle.
         If the puzzle is already in a goal state, simply return []
-    '''
+    """
 
-    ##         "INSERT YOUR CODE HERE"
+    # "INSERT YOUR CODE HERE"
 
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
